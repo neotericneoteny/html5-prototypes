03f7471ae7d3f0d614e7ec504cb824ae41c5829e
Matthew Zimmer <matt@neotericneoteny.com>
2013-02-11 15:56:54 -0500

    cleanup

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Html5FileSystemApi::Application.config.secret_token = 'a2497c474d1122b8b77a2619caf9537b2fe899dc2151a474d6b8423e9d4a8f1fe6fdc345d62d00ca6686647cd461b227b7ae41892bf04087955b44795fae391a'

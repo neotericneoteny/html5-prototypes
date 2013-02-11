53db0bb32feb6278ad9d3f57416756a4fd4864ae
Matthew Zimmer <matt@neotericneoteny.com>
2013-02-11 16:34:37 -0500

    Initial commit after configuring "Twitter Bootstrap, Devise, CanCan" app through Rails Composer.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Html5FileSystemApi::Application.config.secret_token = 'fb4d2f23c22c98013acb496ce24424ee6eafdad7ec4bb355414a23d94df8158efb59370410be4fd6db29dd0556792ef6305476a6591c3409072c15e0d339b0ef'
